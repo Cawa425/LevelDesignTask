0ca575c0cafddd1f057561599131d740d04071ff
Alex Kostan <kostan-sasha@bk.ru>
2023-06-01 15:38:01 +0300

    final

---
+++ Assets/My Project Content/Scripts/SecondEnter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using My_Project_Content.Scripts.Interfaces;
+using UnityEngine;
+
+namespace My_Project_Content.Scripts
+{
+    public class SecondEnter : MonoBehaviour, ITrigger
+    {
+        private void OnCollisionEnter(Collision collision)
+        {
+            MakeAction();
+        }
+
+        public void MakeAction()
+        {
+            MainDemoScript.Instance.mainText.text = "Посетите магазин";
+        }
+    }
+}
